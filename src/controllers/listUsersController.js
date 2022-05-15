f6db72e82f520a26ea0b195a17f1899094d1572a
Filipe Gusmão de Melo <85941319+mghkill@users.noreply.github.com>
2022-05-15 18:04:34 -0300

    Delete src directory

--- src/controllers/listUsersController.js
+++
@@ -1,8 +0,0 @@
-import listUsersServices from "../services/listUsersServices";
-
-const listUsersController = (_, response) => {
-  const user = listUsersServices();
-  return response.status(200).json(user);
-};
-
-export default listUsersController;
--- src/services/getProfileUserService.js
+++
@@ -1,9 +0,0 @@
-import users from "../database";
-
-const getProfileUserService = (email) => {
-  const user = users.find((u) => u.email === email);
-
-  return user;
-};
-
-export default getProfileUserService;
